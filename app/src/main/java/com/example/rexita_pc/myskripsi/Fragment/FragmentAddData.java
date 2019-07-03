1dd7668e64ff0d3e23eedceb0e692411859473ee
Rexita-PC <bragazrexi@gmail.com>
2019-07-03 15:38:21 +0700

    update login user

--- app/src/main/java/com/example/rexita_pc/myskripsi/Fragment/FragmentAddData.java
+++ app/src/main/java/com/example/rexita_pc/myskripsi/Fragment/FragmentAddData.java
@@ -2,11 +2,14 @@ package com.example.rexita_pc.myskripsi.Fragment;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -32,9 +35,13 @@ import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.rexita_pc.myskripsi.Activity.LoginActivity;
+import com.example.rexita_pc.myskripsi.Activity.MenuActivity;
 import com.example.rexita_pc.myskripsi.Model.DataPencurian;
 import com.example.rexita_pc.myskripsi.R;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -63,16 +70,23 @@ import butterknife.ButterKnife;
 import static android.text.TextUtils.isEmpty;
 import static com.android.volley.VolleyLog.TAG;
 
-public class FragmentAddData extends Fragment{
+public class FragmentAddData extends Fragment {
 
-    @BindView(R.id.txtWaktu) TextView txtWaktu;
-    @BindView(R.id.txtTanggal) TextView txtTanggal;
-    @BindView(R.id.txtKejadian) TextView txtKejadian;
-    @BindView(R.id.txtLokasi) TextView txtLokasi;
-    @BindView(R.id.txtKerugian) EditText txtKerugian;
-    @BindView(R.id.btnSubmit) Button btnSubmit;
-    @BindView(R.id.spinnerKecamatan) Spinner spinnerKecamatan;
-
+    @BindView(R.id.txtWaktu)
+    TextView txtWaktu;
+    @BindView(R.id.txtTanggal)
+    TextView txtTanggal;
+    @BindView(R.id.txtKejadian)
+    TextView txtKejadian;
+    @BindView(R.id.txtLokasi)
+    TextView txtLokasi;
+    @BindView(R.id.txtKerugian)
+    EditText txtKerugian;
+    @BindView(R.id.btnSubmit)
+    Button btnSubmit;
+    @BindView(R.id.spinnerKecamatan)
+    Spinner spinnerKecamatan;
+    private FirebaseAuth mAuth;
     List<String> list = new ArrayList<>();
     Calendar c;
 
@@ -81,13 +95,15 @@ public class FragmentAddData extends Fragment{
 
     DatabaseReference databaseReference;
     private String kecamatan = null;
-    String kejadian="";
+    String kejadian = "";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_add_data, container, false);
         //finding listview
         ButterKnife.bind(this, rootView);
+        mAuth = FirebaseAuth.getInstance();
         txtKerugian.addTextChangedListener(onTextChangedListener());
         c = Calendar.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
@@ -117,8 +133,8 @@ public class FragmentAddData extends Fragment{
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 //                        Toast.makeText(getActivity(), "Selected User: "+list.get(position) ,Toast.LENGTH_SHORT).show();
                         String selectedItemText = (String) parent.getItemAtPosition(position);
-                        if (position > 0){
-                            Snackbar.make(getView(), "Applying : " +selectedItemText, Snackbar.LENGTH_SHORT).show();
+                        if (position > 0) {
+                            Snackbar.make(getView(), "Applying : " + selectedItemText, Snackbar.LENGTH_SHORT).show();
                         }
                         kecamatan = list.get(position);
                     }
@@ -167,9 +183,9 @@ public class FragmentAddData extends Fragment{
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                c.set(Calendar.YEAR,year);
-                                c.set(Calendar.MONTH,monthOfYear);
-                                c.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                                c.set(Calendar.YEAR, year);
+                                c.set(Calendar.MONTH, monthOfYear);
+                                c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                                 Date date = new Date();
                                 SimpleDateFormat myFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
                                 String inputString1 = myFormat.format(date);
@@ -179,7 +195,7 @@ public class FragmentAddData extends Fragment{
                                     Date date1 = myFormat.parse(inputString1);
                                     Date date2 = myFormat.parse(inputString2);
                                     long diff = date2.getTime() - date1.getTime();
-                                    Log.d(TAG, "onClick: "+ Math.abs(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)));
+                                    Log.d(TAG, "onClick: " + Math.abs(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)));
                                     interval = Math.abs(TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
                                 } catch (ParseException e) {
                                     e.printStackTrace();
@@ -195,20 +211,34 @@ public class FragmentAddData extends Fragment{
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!isEmpty(txtWaktu.getText().toString()) && !isEmpty(txtTanggal.getText().toString()) && !isEmpty(txtKejadian.getText().toString())
-                        && !isEmpty(txtLokasi.getText().toString()) && !isEmpty(txtKerugian.getText().toString()) && kecamatan != null)
-                {
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if (currentUser == null) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setMessage("Login terlebih dahulu!")
+                            .setCancelable(false)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    //do things
+                                    startActivity(new Intent(getActivity(), LoginActivity.class));
+                                    getActivity().finish();
+                                }
+                            });
+                    AlertDialog alert = builder.create();
+                    alert.show();
+
+                } else if (!isEmpty(txtWaktu.getText().toString()) && !isEmpty(txtTanggal.getText().toString()) && !isEmpty(txtKejadian.getText().toString())
+                        && !isEmpty(txtLokasi.getText().toString()) && !isEmpty(txtKerugian.getText().toString()) && kecamatan != null) {
                     kejadian = txtKejadian.getText().toString();
                     String kerugian = txtKerugian.getText().toString();
                     String names = kerugian.replaceAll(",", "");
                     String waktu = String.valueOf(interval);
                     submitDataPencurian(new DataPencurian(txtWaktu.getText().toString(), txtTanggal.getText().toString(), txtKejadian.getText().toString()
-                            , txtLokasi.getText().toString(), kecamatan,0,names,waktu,0,null));
-                }
-                else{
+                            , txtLokasi.getText().toString(), kecamatan, 0, names, waktu, 0, null));
+                    Toast.makeText(getActivity(), "Applying " + kecamatan, Toast.LENGTH_SHORT).show();
+                } else {
                     Snackbar.make(getView(), "Data tidak boleh kosong", Snackbar.LENGTH_SHORT).show();
                 }
-                Toast.makeText(getActivity(), "Applying "+kecamatan, Toast.LENGTH_SHORT).show();
+
             }
         });
     }
@@ -334,4 +364,5 @@ public class FragmentAddData extends Fragment{
             }
         };
     }
+
 }
--- app/src/main/java/com/example/rexita_pc/myskripsi/Fragment/LogoutFragment.java
+++ app/src/main/java/com/example/rexita_pc/myskripsi/Fragment/LogoutFragment.java
@@ -1,11 +1,13 @@
 package com.example.rexita_pc.myskripsi.Fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +23,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogoutFragment extends Fragment {
 
-        Button btnLogout;
+        Button btnLogout,btnLogin;
         ImageView mImage;
         private FirebaseAuth mAuth;
         private GoogleSignInClient mGoogleSignInClient;
@@ -36,6 +39,8 @@ public class LogoutFragment extends Fragment {
             View rootView = inflater.inflate(R.layout.fragment_logout, container, false);
 
             mImage = (ImageView)rootView.findViewById(R.id.user);
+            btnLogout = (Button)rootView.findViewById(R.id.btnLogout);
+            btnLogin = (Button)rootView.findViewById(R.id.btnLogin);
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                     .requestIdToken(getString(R.string.default_web_client_id))
                     .requestEmail()
@@ -44,9 +49,19 @@ public class LogoutFragment extends Fragment {
             mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
 
             mAuth = FirebaseAuth.getInstance();
-            Uri url = mAuth.getCurrentUser().getPhotoUrl();
-            Glide.with(getActivity()).load(url).thumbnail(0.5f).into(mImage);
-            btnLogout = (Button)rootView.findViewById(R.id.btnLogout);
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            btnLogout.setVisibility(View.INVISIBLE);
+            if (currentUser != null){
+                Uri url = mAuth.getCurrentUser().getPhotoUrl();
+                Glide.with(getActivity()).load(url).thumbnail(0.5f).into(mImage);
+                btnLogout.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+            }else {
+                btnLogout.setVisibility(View.INVISIBLE);
+                btnLogin.setVisibility(View.VISIBLE);
+            }
+
+
 
             return rootView;
         }
@@ -60,20 +75,39 @@ public class LogoutFragment extends Fragment {
                     signOut();
                 }
             });
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(getActivity(), LoginActivity.class));
+                    getActivity().finish();
+                }
+            });
         }
 
         private void signOut() {
             // Firebase sign out
-            mAuth.signOut();
 
-            // Google sign out
-            mGoogleSignInClient.signOut().addOnCompleteListener(getActivity(),
-                    new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            startActivity(new Intent(getActivity(), LoginActivity.class));
-                            getActivity().finish();
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("Apa anda yakin ingin keluar ?")
+                    .setCancelable(false)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //do things
+                            mAuth.signOut();
+                            // Google sign out
+                            mGoogleSignInClient.signOut().addOnCompleteListener(getActivity(),
+                                    new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            startActivity(new Intent(getActivity(), LoginActivity.class));
+                                            getActivity().finish();
+                                        }
+                                    });
                         }
                     });
+            AlertDialog alert = builder.create();
+            alert.show();
+
+
         }
 }
